b84b7208efbeeb532d73d3792170d41dbcdce1bc
ambro <ap40514@student.uni-lj.si>
2025-12-27 16:21:09 +0100

    popravil test.py datoteko - dela

--- python_sql/test.py
+++ python_sql/test.py
@@ -1,11 +1,11 @@
 import sqlite3 as dbapi
 
 # 1. povezav na bazo
-povezva = dbapi.connect("/home//Desktop/Vaje_PodatkovneBaze/python_sql/bbc.db")
+povezva = dbapi.connect("/home/ambro/Desktop/Vaje_PodatkovneBaze/python_sql/narocila.sqlite")
 kazalec = povezva.cursor() 
 
 # 2. izvedba SELECT poizvedebe
-kazalec.execute("SELECT * FROM bbc") #izvede SQL poizvedbo
+kazalec.execute("SELECT * FROM narocilo") #izvede SQL poizvedbo
 
 # 3. prikaz rezultatov 
 rezultati = kazalec.fetchall()  # vrne vrstice
